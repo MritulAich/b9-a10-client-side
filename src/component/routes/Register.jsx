fcd5b95cb15236bf48eb3abb21995099f99f7239
MritulAich <mridulaich332@gmail.com>
2024-05-23 15:34:11 +0600

    register page added

--- src/component/routes/Register.jsx
+++ src/component/routes/Register.jsx
@@ -1,12 +1,44 @@
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../AuthProvider";
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 const Register = () => {
+
+    const { createUser } = useContext(AuthContext);
+
+    const handleRegister = e => {
+        e.preventDefault();
+        const form = new FormData(e.currentTarget);
+        const email = form.get('email');
+        const password = form.get('password');
+        console.log(email, password);
+
+        if (password.length < 6) {
+            alert('Password must contain minimum 6 characters')
+        }
+        else if (!/[A-Z]/.test(password)) {
+            alert('Password must contain at least one uppercase letter')
+        }
+        else if (!/[a-z]/.test(password)) {
+            alert('Password must contain at least one lowercase letter');
+        }
+        else (toast('You have registered successfully'));
+
+
+        createUser(email, password)
+            .then(res => { console.log(res.user) })
+            .catch(err => { console.log(err) })
+    }
+
+
     return (
         <div className="flex flex-col justify-center items-center my-8">
             <div className="w-full max-w-md p-4 rounded-md shadow sm:p-8 bg-gray-50 text-gray-800">
                 <h2 className="mb-3 text-3xl font-semibold text-center">Registration</h2>
 
-                <form>
+                <form onSubmit={handleRegister}>
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text">Name</span>
@@ -33,11 +65,11 @@ const Register = () => {
                     </div>
                     <div className="form-control mt-6">
                     </div>
-                    <button type="button" className="w-full px-8 py-3 mb-4 font-semibold rounded-md bg-blue-600 text-gray-50">Sign up</button>
-                    <h2>Have an account? Go to <Link to='/login' className="font-bold">Login</Link></h2>
+                    <button className="w-full px-8 py-3 mb-4 font-semibold rounded-md bg-blue-600 text-gray-50">Sign up</button>
                 </form>
-                </div>
-
+                    <h2>Have an account? Go to <Link to='/login' className="font-bold">Login</Link></h2>
+            </div>
+            <ToastContainer />
         </div>
     );
 };
